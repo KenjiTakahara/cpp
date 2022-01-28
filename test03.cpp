5f314bc5c32559f8469e782a9572e94a393b5ab9
高原健治 <takahara.ke@gmail.com>
2022-01-29 00:14:28 +0900

    0014

---
+++ test03.cpp
@@ -0,0 +1,11 @@
+#define _USE_MATH_DEFINES
+#include "all.h"
+int main()
+{
+    const char *str = "takahara";
+    for (auto n = 0; n < 9; ++n)
+    {
+        cout << str[n];
+    }
+    cout << endl;
+}
