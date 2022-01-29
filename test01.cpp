c094f573fb1b8cea10f2514399c5755adabedb29
高原健治 <takahara.ke@gmail.com>
2022-01-29 10:25:19 +0900

    Update test01.cpp
    コーヒーのんでる。

--- test01.cpp
+++ test01.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     std::vector<int> v{5, 6, -1, 3, 7, 2, 3};
-    auto a = [](int a, int b) -> auto { return a > b; };
+    auto a = [](int a, int b) -> auto { return a < b; };
     std::sort(std::begin(v), std::end(v), a);
     for (auto e : v)
     {
