aeea02dd35f25c05b53a2d1313319e309b3865a3
高原健治 <takahara.ke@gmail.com>
2022-01-30 18:33:08 +0900

    1833

--- test02.cpp
+++ test02.cpp
@@ -1,5 +1,14 @@
-#include <iostream>
-int main()
+#include <stdio.h>
+
+int main(void)
 {
-    std::cout << sizeof(long long *);
+    puts("Please enter the your family name.");
+    char family_name[40];
+    fgets(family_name, sizeof(family_name), stdin);
+
+    puts("Please enter the your first name.");
+    char first_name[40];
+    fgets(first_name, sizeof(first_name), stdin);
+
+    printf("Hello, %c.%c\n", family_name[0], first_name[0]);
 }
--- test05.cpp
+++ test05.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
-#include <variant>
-#include <string>
-#include "all.h"
 
 int main()
 {
-    std::variant<int, char, std::string> v = "💩";
-    if (std::holds_alternative<std::string>(v))
-    {
-        auto &x = std::get<std::string>(v);
-        std::cout << x << std::endl;
-    }
-    v = 1234;
-    if (std::holds_alternative<int>(v))
-    {
-        auto &x = std::get<int>(v);
-        std::cout << x << std::endl;
-        cout << x * 2;
-    }
+    system("cal -y");
+    return EXIT_SUCCESS;
 }
