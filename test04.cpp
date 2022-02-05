2b4107a34cad26ad0407ac5f502a10f895e29b7f
高原健治 <49593483+KenjiTakahara@users.noreply.github.com>
2022-02-05 14:44:51 +0900

    1414

--- test04.cpp
+++ test04.cpp
@@ -1,29 +1,32 @@
 #include "all.h"
+class X
+{
+    std::vector<int> data_;
+
+public:
+    X() : data_(100, 69) {}
+
+    // *thisが左辺値の場合は、保持しているvectorオブジェクトへの参照を返す
+    const std::vector<int> &data() const &
+    {
+        cout << "& ";
+        return data_;
+    }
+
+    // *thisが右辺値の場合は参照を返すと一時オブジェクトの寿命が尽きてしまうため、
+    // ムーブで返す
+    std::vector<int> data() &&
+    {
+        // return std::move(data_);
+        cout << "&& ";
+        return data_;
+    }
+};
 
 int main()
 {
-    std::vector<int> v = {3, 1, 4};
-
-    // vの全ての要素にラムダ式を適用する
-    std::for_each(v.begin(), v.end(), [](auto x)
-                  { cout << x << endl; });
-
-    cout << "----" << endl;
-
-    // 要素の内容を書き換えても構わないし、呼び出し順序に依存した処理を書いても構わない
-    int n = 0;
-    int m = 0;
-    [&n, &m]() mutable
-    { m++, cout << m; }();
-    [&n, &m]() mutable
-    { m++, cout << m; }();
-    [&n, &m]() mutable
-    { m++, cout << m; }();
-    // std::for_each(v.begin(), v.end(), [n, m](auto &x) mutable
-    //               { x += n++, m++; });
-    // std::for_each(v.begin(), v.end(), [](auto x)
-    //               { cout << x << endl; });
-    cout
-        << "n= " << n << endl;
-    cout << "m= " << m << endl;
+    auto x{X()};
+    const std::vector<int> &v1 = x.data();
+    std::vector<int> v2 = X().data();
+    //  cout << v2.at(99);
 }
