2aa7f2d1793f25de9a687de320d2787ffde624a0
高原健治 <takahara.ke@gmail.com>
2022-02-07 15:30:19 +0900

    1530

---
+++ test14.cpp
@@ -0,0 +1,21 @@
+#include "all.h"
+class CManiac
+{
+    friend int operator+(const CManiac &, const CManiac &);
+    int a;
+
+public:
+    CManiac(int a = 0) : a(a) {}
+};
+
+int operator+(const CManiac &rmaniac1, const CManiac &rmaniac2)
+{
+    return rmaniac1.a + rmaniac2.a;
+}
+
+int main()
+{
+    CManiac c1{2}, c2{5};
+    cout << c1 + c2 << endl;
+    cout << CManiac(3) + CManiac{5} << endl;
+}
