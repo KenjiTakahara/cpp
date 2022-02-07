9a9501dd2c15ade0f6b7d1693b823023f4540ab5
高原健治 <takahara.ke@gmail.com>
2022-02-07 23:40:48 +0900

    2340

--- test16.cpp
+++ test16.cpp
@@ -1,15 +1,18 @@
 #include "all.h"
-int func(int a)
+namespace ton
 {
-    return a;
-}
-int f(const std::function<int(int)> &f,int n)
-{
-    return f(n) * n;
+    int func(int a = 0)
+    {
+        return a;
+    }
+    int f(const std::function<int(int)> &f, int n = 0)
+    {
+        return f(n) * n;
+    }
 }
 
 int main()
 {
-    std::function<int(int)> fn{func};
-    cout << f(fn,4) << endl;
+    std::function<int(int)> fn{ton::func};
+    cout << ton::f(fn, 6) << endl;
 }
