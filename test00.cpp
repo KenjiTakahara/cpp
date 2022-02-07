12d06eda87f960d842aa36f52df339e9b5139d7c
高原健治 <takahara.ke@gmail.com>
2022-02-07 16:50:49 +0900

    1650

--- test00.cpp
+++ test00.cpp
@@ -8,7 +8,8 @@ const char *func()
 
 int main()
 {
-    auto a = 0x1;
-    std::cout << a << std::endl;
-    std::cout << static_cast<std::bitset<32>>(~a) << std::endl;
+    int a = 0b0111;
+    int b = 0b0010;
+    std::cout << (a ^ b) << std::endl;
+    // std::cout << static_cast<std::bitset<32>>(~a) << std::endl;
 }
