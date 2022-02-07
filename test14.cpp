39f01b5c06893c7183595541288d12d834395c8f
高原健治 <takahara.ke@gmail.com>
2022-02-07 16:52:42 +0900

    1652

--- test14.cpp
+++ test14.cpp
@@ -15,7 +15,10 @@ int operator+(const CManiac &rmaniac1, const CManiac &rmaniac2)
 
 int main()
 {
-    CManiac c1{2}, c2{5};
-    cout << c1 + c2 << endl;
-    cout << CManiac(3) + CManiac{5} << endl;
+    std::int8_t i8 = 118;
+    std::uint16_t();
+    // CManiac c1{2}, c2{5};
+    // cout << c1 + c2 << endl;
+    // cout << CManiac(3) + CManiac{5} << endl;
+    cout << (int)i8 << endl;
 }
--- test15.cpp
+++ test15.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 
-namespace my_lib
+void hoge() noexcept
 {
-    class Pair
-    {
-        int a, b;
-
-    public:
-        Pair(int a, int b) : a(a), b(b) {}
-        void show()
-        {
-            std::cout << "{" << a << "," << b << "}" << std::endl;
-        }
-        int sum()
-        {
-            return a + b;
-        }
-    };
+    throw "aabb";
 }
 
 int main()
 {
-    auto pair = my_lib::Pair(4, 6);
-    pair.show();
-    std::cout << pair.sum() << std::endl;
+    try
+    {
+        hoge();
+    }
+    catch (...)
+    {
+        std::cout << "error!";
+    }
     return 0;
 }
