0396389d7d93eaebc036718c2ed029dd18402c31
高原健治 <takahara.ke@gmail.com>
2022-02-21 10:08:17 +0900

    1008

--- ton.cpp
+++ ton.cpp
@@ -1,21 +1,24 @@
-/*様々な実験コード*/
-#pragma once
-#include "all.h"
-auto f01(int a) -> int;
-auto func(void) -> int (*)(int);
-using fp = int (*(*)(void))(int);
-static const fp p = &func;
+// Floating-point_number_precision.c
+// Compile options needed: none. Value of c is printed with a decimal
+// point precision of 10 and 6 (printf rounded value by default) to
+// show the difference
+#include <stdio.h>
+
+#define EPSILON 0.0001 // Define your own tolerance
+#define FLOAT_EQ(x, v) (((v - EPSILON) < x) && (x < (v + EPSILON)))
 
 int main()
 {
-    cout << p()(123);
-}
+    float a, b, c;
 
-auto func(void) -> int (*)(int)
-{
-    return &f01;
-}
-auto f01(int a) -> int
-{
-    return a;
+    a = 1.345f;
+    b = 1.123f;
+    c = a + b;
+    // if (FLOAT_EQ(c, 2.468)) // Remove comment for correct result
+    if (c == 2.468) // Comment this line for correct result
+        printf_s("They are equal.\n");
+    else
+        printf_s("They are not equal! The value of c is %13.20f "
+                 "or %.1f",
+                 c, c);
 }
