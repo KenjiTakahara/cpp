bf5dc5aef453576f44ac32ebabbd911b44006257
高原健治 <takahara.ke@gmail.com>
2022-02-21 14:50:52 +0900

    1450

--- ton.cpp
+++ ton.cpp
@@ -1,24 +1,7 @@
-// Floating-point_number_precision.c
-// Compile options needed: none. Value of c is printed with a decimal
-// point precision of 10 and 6 (printf rounded value by default) to
-// show the difference
-#include <stdio.h>
-
-#define EPSILON 0.0001 // Define your own tolerance
-#define FLOAT_EQ(x, v) (((v - EPSILON) < x) && (x < (v + EPSILON)))
-
+#include "all.h"
 int main()
 {
-    float a, b, c;
-
-    a = 1.345f;
-    b = 1.123f;
-    c = a + b;
-    // if (FLOAT_EQ(c, 2.468)) // Remove comment for correct result
-    if (c == 2.468) // Comment this line for correct result
-        printf_s("They are equal.\n");
-    else
-        printf_s("They are not equal! The value of c is %13.20f "
-                 "or %.1f",
-                 c, c);
+    double a, b, c;
+    a = 1.2l, b = 1.2l, c = a + b;
+    std::cout << std::round(c) << std::endl;
 }
