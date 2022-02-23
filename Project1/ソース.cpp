9081b36693033ca5c001c517325140db84b78bdb
高原健治 <takahara.ke@gmail.com>
2022-02-23 23:14:49 +0900

    2314

--- Project1/ソース.cpp
+++ Project1/ソース.cpp
@@ -1,5 +1,39 @@
-#include <iostream>
-int main()
+#include <iostream>  // cout
+#include <algorithm> // generate
+#include <vector>    // vector
+#include <ctime>     // time
+#include <cstdlib>   //srand,rand
+using namespace std;
+int main(int argc, char const *argv[])
 {
-    std::cout << "test\n";
+    std::srand(unsigned(time(NULL)));
+
+    vector<int> v(8);
+
+    generate(v.begin(), v.end(),
+             []() -> int
+             {
+                 return (rand() % 100);
+             });
+
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    generate(v.begin(), v.end(),
+             []() -> int
+             {
+                 static int i = 0;
+                 return (++i);
+             });
+
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
