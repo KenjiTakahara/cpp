c6def8e22a27b562bfe567379e35e959b40e3df6
高原健治 <takahara.ke@gmail.com>
2022-02-25 22:44:29 +0900

    2244

--- test21.cpp
+++ test21.cpp
@@ -1,16 +1,33 @@
-// #define NDEBUG
-#include <cassert>
 
-void f(int x)
+#include <compare> //<=>利用の場合必須
+#include <iostream>
+
+struct C
 {
-    // パラメータxは、正数でなければならない
-    assert(x >= 0);
+    int x;
+    int y;
+    double v;
+    char str[32];
 
-    // …xを使った処理…
-}
+    //<=>をpublicで定義しておくことで、その他の演算子が導出される
+    auto operator<=>(const C &) const = default;
+};
 
 int main()
 {
-    f(-1); // OK
-           //   f(-1); // プログラムが異常終了する
+    C c1 = {10, 20, 3.1415, "Three-way Comparison"};
+    C c2 = {10, 20, 3.1415, "Spaceship Operator"};
+
+    //三方比較演算子そのものによる比較
+    std::cout << ((c1 <=> c2) == 0) << std::endl;
+    std::cout << ((c1 <=> c2) < 0) << std::endl;
+    std::cout << ((c1 <=> c2) > 0) << std::endl;
+
+    //クラスCは6つの演算子による比較が可能
+    std::cout << (c1 < c2) << std::endl;
+    std::cout << (c1 <= c2) << std::endl;
+    std::cout << (c1 > c2) << std::endl;
+    std::cout << (c1 >= c2) << std::endl;
+    std::cout << (c1 == c2) << std::endl;
+    std::cout << (c1 != c2) << std::endl;
 }
