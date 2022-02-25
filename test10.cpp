2eb7d8dc291e78591a667c7157a2a5b38c5e9ee5
Takahara_Kenji <takahara.ke@gmail.com>
2022-02-26 01:20:08 +0900

    2

--- test10.cpp
+++ test10.cpp
@@ -1,12 +1,19 @@
-#include "all.h"
-
-struct ST
-{
-    ST() { cout << "CONST" << endl; }
-    ~ST() { cout << "DEST" << endl; }
-};
+#include <vector>
+#include <iostream>
 
 int main()
 {
-    std::unique_ptr<ST> ptr(new ST);
+    int arr[4]{};
+    std::cout << std::size(arr) << std::endl;
+
+    std::cout << std::size(u8"arikitari") << std::endl;
+
+    std::vector<int> v = {1, 1, 2, 3, 5, 8};
+    int hoge = 13;
+    v.push_back(hoge);
+    std::cout << std::size(v) << std::endl;
+
+    //変数vの型はstd名前空間にあるクラス型なので
+    // ADLによって `std::`がなくとも発見できる
+    std::cout << size(v) << std::endl;
 }
