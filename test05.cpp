d4b157923076e146563d378ce1bac447d08ae6f8
高原健治 <takahara.ke@gmail.com>
2022-03-01 15:22:32 +0900

    1522

--- test05.cpp
+++ test05.cpp
@@ -3,13 +3,11 @@ template <size_t N>
 void func(const char (&arr)[N])
 {
     for (auto i : arr)
-    {
         cout << i;
-    }
     cout << endl;
 }
 int main()
 {
-    const char ar[] = "abcd";
+    const char ar[] = "1234567890";
     func(ar);
 }
