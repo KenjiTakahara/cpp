c154171039250671c8518e594e0412ffa564475e
高原健治 <takahara.ke@gmail.com>
2022-03-02 08:17:08 +0900

    1

--- test24.cpp
+++ test24.cpp
@@ -1,5 +1,13 @@
 #include "all.h"
+std::vector<int> fn()
+{
+    return {1, 2, 3};
+}
+
 int main()
 {
-    std::nullptr_t n;
+    auto vec{fn()};
+    cout << vec.at(1) << endl;
+    // std::string str1 = "abcde";
+    // std::string str2{std::move(str1)};
 }
