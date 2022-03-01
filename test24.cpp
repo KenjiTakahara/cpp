75b245517c4ee040f6fe07ff7f696589f23cc59a
高原健治 <takahara.ke@gmail.com>
2022-03-01 09:21:45 +0900

    0921

--- test24.cpp
+++ test24.cpp
@@ -1,7 +1,5 @@
 #include "all.h"
 int main()
 {
-    auto v{std::vector<int>{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}};
-    for (auto i = std::begin(v); i != std::end(v); ++i)
-        cout << *i << endl;
+    std::nullptr_t n;
 }
