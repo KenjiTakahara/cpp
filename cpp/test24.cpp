8818270a2b1b5b3c81f2d82131f48cc4fdc5ed18
高原健治 <49593483+KenjiTakahara@users.noreply.github.com>
2022-03-03 17:29:56 +0900

    unkositakarane

--- cpp/test24.cpp
+++ cpp/test24.cpp
@@ -1,38 +1,40 @@
 #include "all.h"
-using namespace std;
-class T24
+
+class Base
 {
-    const int a, b, c;
+    virtual const char *eat() = 0;
+};
+
+class Deli : public Base
+{
+    bool bl;
 
 public:
-    T24(int a, int b, int c)
-        : a(a), b(b), c(c) {}
-    friend ostream &operator<<(ostream &, T24 &);
-    friend ostream &operator<<(ostream &, const T24 &&);
-};
-
-ostream &operator<<(ostream &s, T24 &t)
-{
-    return s << (t.a * t.b * t.c);
-};
-
-ostream &operator<<(ostream &s, const T24 &&t)
-{
-    return s << (t.a + t.b + t.c);
-};
+    Deli(bool bl) : bl(bl) {}
+    const char *eat() override final
+    {
+        const char *kuso = "💩";
+        const char *app = "🍎";
+        return (bl ? kuso : app);
+    }
+    explicit operator bool()
+    {
+        if (bl)
+            return true;
+        else
+            return false;
+    }
+    operator int()
+    {
+        if (bl)
+            return 2;
+        else
+            return 0;
+    }
+} obj = true;
 
 int main()
 {
-    T24 test01{2, 3, 4};
-    cout << test01 << endl;
-    cout << std::move(test01) << endl;
-    cout << T24{1, 2, 3} << endl;
-    char str[1];
-    cout << std::end(str) - std::begin(str);
-#include <limits>
-    constexpr float f = std::numeric_limits<float>::infinity();
-    constexpr double d = std::numeric_limits<double>::infinity();
-
-    std::cout << "float : " << f << std::endl;
-    std::cout << "double : " << d << std::endl;
+    cout << obj.eat() << endl;
+    // obj ? cout << "true" << endl : cout << "false" << endl;
 }
